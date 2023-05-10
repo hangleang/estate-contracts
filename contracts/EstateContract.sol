4d691650e41dac0592fe33e71efb441b1258566d
hangleang <hangleang127@gmail.com>
2023-05-10 16:15:12 +0700

    feat: create estate contract

---
+++ contracts/EstateContract.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "./extensions/ERC4907.sol";
+
+contract EstateContract is ERC4907, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable, ERC721Burnable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor(string memory name_, string memory symbol_) ERC4907(name_, symbol_) {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, string memory uri) public {
+        uint256 _tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, _tokenId);
+        _setTokenURI(_tokenId, uri);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 _tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC4907, ERC721Enumerable) whenNotPaused {
+        super._beforeTokenTransfer(from, to, _tokenId, batchSize);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 _tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(_tokenId);
+    }
+
+    function tokenURI(uint256 _tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(_tokenId);
+    }
+
+    function tokenId() public view returns (uint256) {
+        return _tokenIdCounter.current();
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC4907, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
