d2c7948ef15f061bd89891bcb58b7702ec4c2431
hangleang <hangleang127@gmail.com>
2023-05-12 14:55:40 +0700

    test: update test script

--- test/estate.test.ts
+++ test/estate.test.ts
@@ -59,6 +59,8 @@ describe("Estate Contract testcase", function () {
         )
           .to.emit(this.estate, "NFTSale")
           .withArgs(this.accounts[1].address, this.accounts[2].address, sale.price, idx, sale.uri);
+
+        expect(await this.estate.ownerOf(idx)).to.eq(this.accounts[2].address);
       });
     });
   });
@@ -195,7 +197,98 @@ describe("Estate Contract testcase", function () {
         )
           .to.emit(this.estate, "NFTRent")
           .withArgs(this.accounts[1].address, this.accounts[2].address, totalPrice, idx, rent.uri, expiredAt);
+
+        expect(await this.estate.ownerOf(idx)).to.eq(this.accounts[1].address);
+        expect(await this.estate.userOf(idx)).to.eq(this.accounts[2].address);
       });
+    });
+  });
+
+  describe("Frontrun", function () {
+    before(async function () {
+      await deployments.fixture(["EstateContract"]);
+      this.estate = await getContract<EstateContract>("EstateContract");
+      this.name = await this.estate.name();
+
+      // list sale #0
+      this.firstRent = rental[0];
+      this.firstRent.nonce = await this.accounts[1].getTransactionCount("latest");
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { rentDuration, ...rentInfo } = this.firstRent;
+      this.firstRent.signature = await this.accounts[1]._signTypedData(
+        // Domain
+        {
+          name: this.name,
+          version: "1.0.0",
+          chainId: this.chainId,
+          verifyingContract: this.estate.address,
+        },
+        // Types
+        {
+          NFTForRentWithMint: [
+            { name: "lister", type: "address" },
+            { name: "pricePerUnit", type: "uint256" },
+            { name: "timeUnit", type: "uint64" },
+            { name: "minDuration", type: "uint64" },
+            { name: "maxDuration", type: "uint64" },
+            { name: "uri", type: "string" },
+            { name: "nonce", type: "uint256" },
+          ],
+        },
+        // Value
+        { ...rentInfo, lister: this.accounts[1].address },
+      );
+    });
+
+    it("mint with wrong signer - failure", async function () {
+      const totalPrice = ethers.BigNumber.from(this.firstRent.pricePerUnit)
+        .mul(this.firstRent.rentDuration)
+        .div(this.firstRent.timeUnit);
+      await expect(
+        this.estate
+          .connect(this.accounts[2])
+          .rentWithMint(
+            this.accounts[2].address,
+            this.accounts[0].address,
+            this.firstRent.pricePerUnit,
+            this.firstRent.timeUnit,
+            this.firstRent.minDuration,
+            this.firstRent.maxDuration,
+            this.firstRent.rentDuration,
+            this.firstRent.uri,
+            this.firstRent.nonce,
+            this.firstRent.signature,
+            {
+              value: totalPrice,
+            },
+          ),
+      ).to.be.revertedWith("Invalid/Used signature");
+    });
+
+    it("list/mint with the same account - failure", async function () {
+      const totalPrice = ethers.BigNumber.from(this.firstRent.pricePerUnit)
+        .mul(this.firstRent.rentDuration)
+        .div(this.firstRent.timeUnit);
+      await expect(
+        this.estate
+          .connect(this.accounts[1])
+          .rentWithMint(
+            this.accounts[1].address,
+            this.accounts[1].address,
+            this.firstRent.pricePerUnit,
+            this.firstRent.timeUnit,
+            this.firstRent.minDuration,
+            this.firstRent.maxDuration,
+            this.firstRent.rentDuration,
+            this.firstRent.uri,
+            this.firstRent.nonce,
+            this.firstRent.signature,
+            {
+              value: totalPrice,
+            },
+          ),
+      ).to.be.revertedWith("Invalid address");
     });
   });
 });
